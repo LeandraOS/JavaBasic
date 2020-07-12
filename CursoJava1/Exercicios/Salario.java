5362f98712976b31446e604d49f2b566e2f03f1c
LeandraOliveiraS <leandra.silva@ccc.ufcg.edu.br>
2020-07-12 00:14:27 -0300

    :pencil:  Adição de Salario.java

---
+++ CursoJava1/Exercicios/Salario.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Salario {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite o valor do salário base: ");
+        double salarioBase = scan.nextDouble();
+        double gratificacao = salarioBase * 0.05;
+        double imposto = salarioBase * 0.07;
+        double salarioReceber = salarioBase + gratificacao - imposto;
+        System.out.printf("\nValor da gratificação: R$%.2f \nValor do imposto: R$%.2f \nValor do salário a receber: R$%.2f \n", gratificacao, imposto, salarioReceber);
+
+
+    }
+
+
+}
