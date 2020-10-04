3676f877e708457ae5d0817e8515ef6af1d64112
LeandraOliveiraS <leandra.silva@ccc.ufcg.edu.br>
2020-10-04 19:10:00 -0300

    adiciona novos exercicios de EDA

--- CursoJava1/EDA/BubbleSort.java
+++ CursoJava1/EDA/BubbleSort.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class BubbleSort{
     public static int[] bubbleSort(int [] array){
@@ -17,8 +16,7 @@ public class BubbleSort{
 
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int [] entrada = {9,8,7,6,5,4,3,2,1,0};
-        System.out.println(Arrays.toString(bubbleSort(entrada)));
+        int [] array = {9,8,7,6,5,4,3,2,1,0}
+        System.out.println(Arrays.toString(bubbleSort(array)));
     }
 }
